47d4fa57099d5031dd9b0b88c3be87f903f7c02d
sagaramlokesh1 <118746459+sagaramlokesh1@users.noreply.github.com>
2023-09-25 00:07:17 +0530

    Add files via upload

---
+++ otp/script.js
@@ -0,0 +1,10 @@
+function generatorOTP() {
+    var digits = "0123456789";
+    let OTP = "";
+
+    for (let i = 0; i < 6; i++) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    var span = document.querySelector('.box span');
+    span.innerHTML = OTP;
+}
